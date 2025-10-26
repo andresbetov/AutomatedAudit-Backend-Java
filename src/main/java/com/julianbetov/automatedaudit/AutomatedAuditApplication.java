73e7f8c35d49f9e40dbe15089a376a3e15c2bf59
Julian Bermudez <julianbetov@gmail.com>
2025-10-25 19:56:51 -0500

    enable Spring Data web support for DTO serialization in the application

--- src/main/java/com/julianbetov/automatedaudit/AutomatedAuditApplication.java
+++ src/main/java/com/julianbetov/automatedaudit/AutomatedAuditApplication.java
@@ -2,8 +2,10 @@ package com.julianbetov.automatedaudit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AutomatedAuditApplication {
 
     public static void main(String[] args) {
